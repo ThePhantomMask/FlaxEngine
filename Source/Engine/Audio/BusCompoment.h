c86f7db8d834fff67e7ed9ab3d811c2af3542969
Phantom <salvini.michael01@gmail.com>
2025-07-10 19:03:52 +0200

    Create BusCompoment

---
+++ Source/Engine/Audio/BusCompoment.h
@@ -0,0 +1,30 @@
+// Copyright (c) Wojciech Figat. All rights reserved.
+
+#pragma once
+
+#include "Engine/Core/Types/String.h"
+#include "Engine/Core/ISerializable.h"
+
+API_STRUCT() struct BusCompoment : ISerializable
+{
+    API_AUTO_SERIALIZATION();
+    DECLARE_SCRIPTING_TYPE_MINIMAL(BusCompoment);
+
+    /// <summary>
+    ///  Name of Audio Bus
+    /// </summary>
+    API_FIELD(Attributes = "EditorOrder(10)")
+    String Name;
+
+    /// <summary>
+    /// Volume of Audio Bus (which will be converted to decibel scale)
+    /// </summary>
+    API_FIELD(Attributes = "EditorOrder(20), Limit(0, 1)")
+    float Volume = 1;
+    
+    /// <summary>
+    /// If Audio Bus is muted or not
+    /// </summary>
+    API_FIELD(Attributes = "EditorOrder(30)")
+    bool IsMuted;
+};
